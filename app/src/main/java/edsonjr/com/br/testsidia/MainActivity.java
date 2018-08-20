f43f1eb3f54761a792d1628af5a040a69b92124c
Edson Jr <eds_jr@hotmail.com>
2018-08-20 19:38:00 -0400

    First Commit
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ app/src/main/java/edsonjr/com/br/testsidia/MainActivity.java
@@ -0,0 +1,13 @@
+package edsonjr.com.br.testsidia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
